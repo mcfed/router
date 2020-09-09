37c8a340d7a1ad916c5e016dd36f3c2100fe1757
Jshoes <576504437@qq.com>
2020-09-09 16:04:53 +0800

    feat: feat react spaRouter

--- src/react/component/Provider.tsx
+++ src/react/component/Provider.tsx
@@ -1,27 +1,36 @@
-import React, { Children } from 'react'
-import { SPARouter } from '../../router';
-import { RouterView } from './RouterView';
+import React, { Children, useState } from "react";
+import { ReactRoute } from "./Route";
 
+export const RouterProvider = React.createContext({} as any);
 
-// export const SPAProviderContext = React.createContext({});
-
-
-interface SPAProviderProp {
-  spaRouter: SPARouter;
-  children: React.ReactNode;
+interface RouteProps {
+  router: any;
 }
 
-export function SPAProvider(prop: SPAProviderProp) {
-  // const child = Children.only(prop.children)
-  // console.log(child)
-  /*
-  return (<SPAProviderContext.Provider value={prop.spaRouter}>
-        { 
-          //@ts-ignore
-          React.createElement(child.type,{spaRouter:prop.spaRouter})
-        }
-        </SPAProviderContext.Provider>)
-        */
-
-  return <RouterView spaRouter={prop.spaRouter}></RouterView>
+export function ReactRouter(props: RouteProps) {
+  return (
+    <RouterProvider.Provider value={{ router: props.router }}>
+      <ReactRoute></ReactRoute>
+    </RouterProvider.Provider>
+  );
 }
+
+// interface SPAProviderProp {
+//   spaRouter: SPARouter;
+//   children: React.ReactNode;
+// }
+
+// export function SPAProvider(prop: SPAProviderProp) {
+//   // const child = Children.only(prop.children)
+//   // console.log(child)
+//   /*
+//   return (<SPAProviderContext.Provider value={prop.spaRouter}>
+//         {
+//           //@ts-ignore
+//           React.createElement(child.type,{spaRouter:prop.spaRouter})
+//         }
+//         </SPAProviderContext.Provider>)
+//         */
+
+//   return <RouterView spaRouter={prop.spaRouter}></RouterView>
+// }
